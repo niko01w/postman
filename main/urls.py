1befc4ccb00b8ad0558eba9ef58673788f8d3fca
niko01w <niko01w@mail.ru>
2022-10-06 11:30:46 +0600

    first commit

---
+++ main/urls.py
@@ -0,0 +1,15 @@
+
+
+from django.urls import path
+
+from main import views
+
+urlpatterns = [
+    path('categories/', views.CategoryListView.as_view()),
+    path('posts/', views.PostListCreateView.as_view()),
+    path('posts/<int:pk>/', views.PostDetailView.as_view()),
+    path('comments/', views.CommentListCreateView.as_view()),
+    path('comments/<int:pk>/', views.CommentDetailView.as_view()),
+    # path('categories/', views.category_list),
+    # path('categories1/', views.CategoryListView.as_view()),
+]
